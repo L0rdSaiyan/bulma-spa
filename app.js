0d2f4f6f89206f57cff8be9d9b144146d55ca744
joao victor <thehoodgamer20@gmail.com>
2022-09-04 15:58:50 -0300

    adicionado alertify às outras páginas e funcionalidades novas

--- app.js
+++ app.js
@@ -1,47 +1,65 @@
-function cadastrar(){
+function cadastrar() {
 
     let nome = document.getElementById("usuario").value
     let senha = document.getElementById("senha").value
     let confirm = document.getElementById("confirm").value
-    let msg = document.getElementById("msg")
-    let msg2 = document.getElementById("msg2")
 
-    if(senha!==confirm){
+    if (senha !== confirm) {
 
-        msg2.innerText = "As senhas não conferem!"
+        alertify.error('As senhas não conferem!')
 
     }
 
+    else if (nome == "" || senha == "" || confirm == "") {
 
-    else if(nome=="" || senha=="" || confirm==""){
+        alertify.error('Não são permitidos campos em branco!')
 
-        msg.innerText = "Não são permitidos campos em branco!"
+    } else if (senha.length < 6) {
+
+        alertify.error('As senhas devem conter no mínimo 6 dígitos!')
 
     }
 
-    else{
+    else {
 
-        location.href="index.html"
+        alertify.alert('Conta criada com sucesso!', `Bem-vindo, ${nome}!`)
+
+        setTimeout(redirect => {
+
+            location.href = "index.html"
+
+
+        }, 2000)
+
 
     }
 
 }
 
-function confirm(){
+function confirm() {
 
-let usuario = document.getElementById("usuario").value
-let senha = document.getElementById("senha").value
+    let usuario = document.getElementById("usuario").value
+    let senha = document.getElementById("senha").value
 
-if(usuario == "" || senha == ""){
+    if (usuario == "" || senha == "") {
 
-    alertify.error('preencha os campos corretamente!')
+        alertify.error('Preencha os campos corretamente!')
 
-}else{
 
-    window.location.href="index.html"
+    } else if (senha < 6) {
 
+        alertify.error('As senhas devem conter 6 dígitos!')
+
+    } else {
+
+        alertify.alert('Login bem sucedido', `Bem vindo, ${usuario}`)
+
+        setTimeout(redirect => {
+
+            window.location.href = "index.html"
+
+
+        }, 2000)
+
+    }
 }
-
-
-}
-
